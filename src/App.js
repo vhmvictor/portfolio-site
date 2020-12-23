c2932239345e0b44b81ccaba92ab342a5f8bab95
vhmvictor <victor.hugomarques@hotmail.com>
2020-12-23 19:24:31 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,257 @@
+import './App.css';
+import { FaGithub, FaLinkedin, FaUserAlt, FaEnvelope } from 'react-icons/fa';
+import { ReactComponent as WorkIcon } from "./work.svg";
+import { ReactComponent as SchoolIcon } from "./school.svg";
+import timelineElements from "./timelineElements";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+
+function App() {
+
+  let workIconStyles = { background: "#06D6A0" };
+  let schoolIconStyles = { background: "#f9c74f" };
+
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="max-width">
+          <div className="logo"><a href="#"> Meu <span>Portifólio</span></a></div>
+          <ul className="menu">
+            <li><a href="#about" className="menu-btn">Sobre</a></li>
+            <li><a href="#services" className="menu-btn">Experiências Profissionais / Especializações</a></li>
+            <li><a href="#skills" className="menu-btn">Habilidades</a></li>
+            <li><a href="#contact" className="menu-btn">Contato</a></li>
+          </ul>
+          <div className="menu-btn">
+            <i className="fas fa-bars" />
+          </div>
+        </div>
+      </nav>
+      {/* principal */}
+      <section className="home" style={{justifyContent: 'center'}} id="home">
+        <div className="max-width">
+          <div className="home-content">
+            <div className="text-1">Olá, meu nome é</div>
+            <div className="text-2">Victor Hugo Marques</div>
+            <div className="text-3">e sou um Desenvolvedor FullStack</div>
+          </div>
+          <div style={{display: 'flex', justifyContent: 'center'}}> 
+            {/* Github */}
+            <div style={{padding: '20px'}}>
+              <a style={{fontSize: '50px', color: 'white'}} href="https://github.com/vhmvictor" target="_blank"><FaGithub /></a>
+            </div>
+            {/* LinkedIn */} 
+            <div style={{padding: '20px'}}>
+              <a style={{fontSize: '50px', color: 'white'}} href="https://www.linkedin.com/in/vhmvictor/" target="_blank"><FaLinkedin /></a>
+            </div>
+          </div>
+        </div>
+        <div className="fullstack-img">
+          <a href="http://google.com"><img width={500} src="https://www.hostinger.com.br/tutoriais/wp-content/uploads/sites/12/2018/05/20-sites-para-aprender-como-programar-de-gra%C3%A7a-1280x720.png" alt=""/></a>
+        </div>
+      </section>
+      {/* sobre */}
+      <section className="about" id="about">
+        <div className="max-width">
+          <h2 className="title">Sobre</h2>
+          <div className="about-content">
+            <div className="column left">
+              <img src="https://avatars0.githubusercontent.com/u/51443108?s=460&u=d15d6338a528381285ff58cc27ad360e415c98b1&v=4" alt="" />
+            </div>
+            <div className="column right">
+              <div className="text">Desenvolvedor/Estudante/Atleta nas horas vagas</div>
+              <p>Estudante de Engenharia de Computação, me especializando em todo ecossistema da linguagem de programação JavaScript, buscando mais conhecimento na área e trabalhando para me tornar um excelente Full Stack Developer. Fascinado por tecnoligias e tudo que envolve o meio. Atualmente aprimorando meus conhecimentos nas tecnologias: Node.js / Python, React.js e React Native </p>
+              <a href="#">Download CV</a>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* timeline */}
+      <section className="services" id="services">
+        <div className="vertical-timeline-component">
+          <h2 className="title">Timeline</h2>
+          <div>
+            <VerticalTimeline>
+              {timelineElements.map((element) => {
+                let isWorkIcon = element.icon === "work";
+                let showButton =
+                  element.buttonText !== undefined &&
+                  element.buttonText !== null &&
+                  element.buttonText !== "";
+
+                return (
+                  <VerticalTimelineElement
+                    key={element.key}
+                    date={element.date}
+                    dateClassName="date"
+                    iconStyle={isWorkIcon ? workIconStyles : schoolIconStyles}
+                    icon={isWorkIcon ? <WorkIcon /> : <SchoolIcon />}
+                  > 
+                    <h3 style={{color:'black'}} className="vertical-timeline-element-title">
+                      {element.title}
+                    </h3>
+                    <h5 style={{color:'black'}} className="vertical-timeline-element-subtitle">
+                      {element.location}
+                    </h5>
+                    <p style={{color:'black'}} id="description">{element.description}</p>
+                    {showButton && (
+                      <a
+                        className={`button ${
+                          isWorkIcon ? "workButton" : "schoolButton"
+                        }`}
+                        href="/"
+                      >
+                        {element.buttonText}
+                      </a>
+                    )}
+                  </VerticalTimelineElement>
+                );
+              })}
+            </VerticalTimeline>
+          </div>
+        </div>
+      </section>
+      {/* habilidades */}
+      <section className="skills" id="skills">
+        <div className="max-width">
+          <h2 className="title">Minhas habilidades</h2>
+          <div className="skills-content">
+            <div className="column left">
+              <div className="text">Minhas habilidades &amp; experiências criativas.</div>
+              <p>Eu procuro sempre estar me atualizando com as novas tecnologias que são desenvolvidas e que estão em alta no mundo do desenvolvimento, afim de sempre estar atualizado para o mercado de trabalho e me tornar um profissional mais completo possível. Ao lado, apresento uma classificação das minhas habilidades com diferentes tecnologias que foram desenvolvidas ao longo da minha carreira profissional e com participações em WorkShops e cursos de especialização.</p>
+            </div>
+            <div className="column right">
+              <div className="bars">
+                <div className="info">
+                  <span>HTML</span>
+                  <span>90%</span>
+                </div>
+                <div className="line html" />
+              </div>
+              <div className="bars">
+                <div className="info">
+                  <span>CSS</span>
+                  <span>70%</span>
+                </div>
+                <div className="line css" />
+              </div>
+              <div className="bars">
+                <div className="info">
+                  <span>JavaScript</span>
+                  <span>90%</span>
+                </div>
+                <div className="line js" />
+              </div>
+              <div className="bars">
+                <div className="info">
+                  <span>ReactJs</span>
+                  <span>70%</span>
+                </div>
+                <div className="line reactjs" />
+              </div>
+              <div className="bars">
+                <div className="info">
+                  <span>NodeJs</span>
+                  <span>90%</span>
+                </div>
+                <div className="line nodejs" />
+              </div>
+              <div className="bars">
+                <div className="info">
+                  <span>Python</span>
+                  <span>70%</span>
+                </div>
+                <div className="line python" />
+              </div>
+              <div className="bars">
+                <div className="info">
+                  <span>PHP</span>
+                  <span>50%</span>
+                </div>
+                <div className="line php" />
+              </div>
+              <div className="bars">
+                <div className="info">
+                  <span>MySQL</span>
+                  <span>80%</span>
+                </div>
+                <div className="line mysql" />
+              </div>
+              <div className="bars">
+                <div className="info">
+                  <span>PostgreSQL</span>
+                  <span>60%</span>
+                </div>
+                <div className="line postgress" />
+              </div>
+              <div className="bars">
+                <div className="info">
+                  <span>PowerBI</span>
+                  <span>80%</span>
+                </div>
+                <div className="line powerbi" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* contato */}
+      <section className="contact" id="contact">
+        <div className="max-width">
+          <h2 className="title">Contato</h2>
+          <div className="contact-content">
+            <div className="column left">
+              <div className="text">Projetos / Sugestões / Oportunidades</div>
+              <p>Você tem alguma ideia que gostaria de colocar em prática ? Entre em contato por e-mail e vamos discutir melhor!</p>
+              <div className="icons">
+                <div className="row">
+                <i style={{fontSize: '30px', color: 'crimson'}}> <FaUserAlt /></i>
+                  <div className="info">
+                    <div className="head">Victor Hugo Marques</div>
+                  </div>
+                </div>
+                <div className="row">
+                <i style={{fontSize: '30px', color: 'crimson'}}> <FaEnvelope /></i>
+                  <div className="info" style={{justifyItems: 'center'}}>
+                    <div className="head">victor.hugomarques@hotmial.com</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="column right">
+              <div className="text">Me envie um e-mail!</div>
+              <form action="#">
+                <div className="fields">
+                  <div className="field name">
+                    <input type="text" placeholder="Nome" required />
+                  </div>
+                  <div className="field email">
+                    <input type="email" placeholder="Email" required />
+                  </div>
+                </div>
+                <div className="field">
+                  <input type="text" placeholder="Assunto" required />
+                </div>
+                <div className="field textarea">
+                  <textarea cols={30} rows={10} placeholder="Mensagem..." required defaultValue={""} />
+                </div>
+                <div className="button">
+                  <button type="submit">Enviar</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* fim */}
+      <footer>
+        <span>Created By <span style={{color: 'crimson'}}> Victor Hugo Marques </span> | 2020 All rights reserved.</span>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
